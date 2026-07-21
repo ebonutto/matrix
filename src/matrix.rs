fba224f718d8fec9bc39aa418b637b9173a284b9
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-21 17:15:54 +0200

    feat: start matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,50 +1,127 @@
-pub struct Matrix {
-	rows: usize,
-	cols: usize,
-	data: Vec<Vec<f64>>,
+use std::fmt;
+use std::ops::{Index, IndexMut};
+
+pub struct Matrix<K> {
+    rows: usize,
+    cols: usize,
+    data: Vec<K>,
 }
 
-impl Matrix {
-	#[allow(dead_code)]
-	pub fn new( rows: usize, cols: usize, data: Vec<Vec<f64>> ) -> Matrix {
-		assert!( rows > 0, "Matrix must have at least one row" );
-		assert!( cols > 0, "Matrix must have at least one column" );
-		assert_eq!( data.len(), rows, "Row count mismatch" );
-		assert!( data.iter().all( |row| row.len() == cols ), "Column count mismatch in at least one row" );
-		Matrix { rows, cols, data }
-	}
+impl<K> Matrix<K> {
+    pub fn rows(&self) -> usize {
+        self.rows
+    }
 
-	#[allow(dead_code)]
-	pub fn zeros( rows: usize, cols: usize ) -> Matrix {
-		assert!( rows > 0, "Matrix must have at least one row" );
-		assert!( cols > 0, "Matrix must have at least one column" );
-		let data = vec![vec![0.0; cols]; rows];
-		Matrix { rows, cols, data }
-	}
+    pub fn cols(&self) -> usize {
+        self.cols
+    }
 
-	#[allow(dead_code)]	
-	pub fn shape( &self ) -> ( usize, usize ) {
-		( self.rows, self.cols )
-	}
+    pub fn shape(&self) -> (usize, usize) {
+        (self.rows, self.cols)
+    }
 
-	#[allow(dead_code)]	
-	pub fn is_square( &self ) -> bool {
-		self.rows == self.cols
-	}
-
-	#[allow(dead_code)]	
-	pub fn print( &self ) {
-		for row in &self.data {
-			println!( "{:?}", row );
-		}
-	}
-
-
+    pub fn is_square(&self) -> bool {
+        self.rows == self.cols
+    }
 }
 
+// // From 1D
+// impl<K, const N: usize> From<[K; N]> for Matrix<K> {
+//     fn from(data: [K; N]) -> Self {
+//         Self {
+//             rows: 1,
+//             cols: N,
+//             data: data.into_iter().collect(),
+//         }
+//     }
+// }
 
-// use std::fmt;
+// From 2D
+impl<K, const R: usize, const C: usize> From<[[K; C]; R]> for Matrix<K> {
+    fn from(data: [[K; C]; R]) -> Self {
+        Self {
+            rows: R,
+            cols: C,
+            data: data.into_iter().flatten().collect(),
+        }
+    }
+}
 
-// impl fmt::Display for Matrix {
-// 	fn fmt( &self, f:)
-// } 
+//TODO Improve display (see subject)
+impl<K: fmt::Display> fmt::Display for Matrix<K> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in 0..self.rows {
+            write!(f, "[")?;
+
+            for col in 0..self.cols {
+                write!(f, "{}", self.data[row * self.cols + col])?;
+
+                if col + 1 < self.cols {
+                    write!(f, ", ")?;
+                }
+            }
+
+            writeln!(f, "]")?;
+        }
+        Ok(())
+    }
+}
+
+impl<K> Index<(usize, usize)> for Matrix<K> {
+    type Output = K;
+
+    fn index(&self, (row, col): (usize, usize)) -> &Self::Output {
+        debug_assert!(row < self.rows && col < self.cols, "index out of bounds");
+        &self.data[row * self.cols + col]
+    }
+}
+
+impl<K> IndexMut<(usize, usize)> for Matrix<K> {
+    fn index_mut(&mut self, (row, col): (usize, usize)) -> &mut Self::Output {
+        debug_assert!(row < self.rows && col < self.cols, "index out of bounds");
+        &mut self.data[row * self.cols + col]
+    }
+}
+
+impl<K> Matrix<K>
+where
+    K: Copy + std::ops::AddAssign,
+{
+    pub fn add(&mut self, v: &Matrix<K>) {
+        assert_eq!(self.shape(), v.shape(), "FUCK",);
+
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                self[(row, col)] += v[(row, col)];
+            }
+        }
+    }
+}
+
+impl<K> Matrix<K>
+where
+    K: Copy + std::ops::SubAssign,
+{
+    pub fn sub(&mut self, v: &Matrix<K>) {
+        assert_eq!(self.shape(), v.shape(), "FUCK",);
+
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                self[(row, col)] -= v[(row, col)];
+            }
+        }
+    }
+}
+
+impl<K> Matrix<K>
+where
+    K: Copy + std::ops::MulAssign,
+{
+    pub fn scl(&mut self, a: K) {
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                self[(row, col)] *= a;
+            }
+        }
+    }
+}
