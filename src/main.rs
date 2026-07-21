1d63d711aeb67cdcabbb81aa938560007c841fc5
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-21 17:32:17 +0200

    feat: add main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-mod matrix;
-
 use matrix::Matrix;
 
 fn main() {
-    let matrix = Matrix::zeros(3, 1);
-    matrix.print();
+    let mut m1 = Matrix::from([[1, 2], [3, 4]]);
+    let mut m2 = Matrix::from([[5, 6], [7, 8]]);
+
+    println!("{}", m1);
+    println!("{}", m2);
+
+    m1.add(&m2);
+    println!("{}", m1);
 }
