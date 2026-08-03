15d18417a75cf9572133ff538035cb62d69cb636
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-08-03 11:04:56 +0200

    feat: add exercices

--- examples/ex01_linear_combination.rs
+++ examples/ex01_linear_combination.rs
@@ -5,14 +5,14 @@ fn main() {
     let e2 = Vector::from([0., 1., 0.]);
     let e3 = Vector::from([0., 0., 1.]);
     println!("{}", linear_combination(&[e1, e2, e3], &[10., -2., 0.5]));
-    // [10.]
-    // [-2.]
+    // [10.0]
+    // [-2.0]
     // [0.5]
 
     let v1 = Vector::from([1., 2., 3.]);
     let v2 = Vector::from([0., 10., -100.]);
     println!("{}", linear_combination(&[v1, v2], &[10., -2.]));
-    // [10.]
-    // [0.]
-    // [230.]
+    // [10.0]
+    // [0.0]
+    // [230.0]
 }
--- examples/ex06_cross_product.rs
+++ examples/ex06_cross_product.rs
@@ -4,21 +4,21 @@ fn main() {
     let u = Vector::from([0., 0., 1.]);
     let v = Vector::from([1., 0., 0.]);
     println!("{}", cross_product(&u, &v));
-    // [0.]
-    // [1.]
-    // [0.]
+    // [0.0]
+    // [1.0]
+    // [0.0]
 
     let u = Vector::from([1., 2., 3.]);
     let v = Vector::from([4., 5., 6.]);
     println!("{}", cross_product(&u, &v));
-    // [-3.]
-    // [6.]
-    // [-3.]
+    // [-3.0]
+    // [6.0]
+    // [-3.0]
 
     let u = Vector::from([4., 2., -3.]);
     let v = Vector::from([-2., -5., 16.]);
     println!("{}", cross_product(&u, &v));
-    // [17.]
-    // [-58.]
-    // [-16.]
+    // [17.0]
+    // [-58.0]
+    // [-16.0]
 }
